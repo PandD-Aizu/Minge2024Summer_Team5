6d019f84eb75a2cb3bbe9043482f7332b03878bc
wasabi <hayato-f@i.softbank.jp>
2024-08-29 14:06:06 +0900

    マジックナンバーを変数化した。

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int moveDistance = 3;
+    public float movespeed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.PingPong(Time.time, 1) * 2f + 1, transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.PingPong(Time.time, moveDistance) * movespeed + moveDistance, transform.position.y, transform.position.z);
       //transform.position = new Vector3(Mathf.PingPong(Time.time, ˆÚ“®‹——£) * ˆÚ“®‘¬“x + ‰ŠúˆÊ’u, transform.position.y, transform.position.z);
     }
 }
