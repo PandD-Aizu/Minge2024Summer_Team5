b0eff3d04f37e22504fb3ef67091294aa7ab5df9
wasabi <hayato-f@i.softbank.jp>
2024-09-08 16:27:46 +0900

    Error解消

--- Assets/Scripts/Piano_ChangeSpeedAndJumpPower.cs
+++ Assets/Scripts/Piano_ChangeSpeedAndJumpPower.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Piano_ChangeSpeedAndJumpPower : MonoBehaviour{
-
+    public Player Player;
     public int pianochange_speed = 0;/*切り替えスイッチ*/
     public int pianochange_jumppower = 0;
     float speed_p = 15f;    /*変換後の速度*/
     int jumppower_p = 350;  /*変換後のジャンプ力*/
-    
-    if(pianochange_speed == 1){
-        Player.speed = speed_p;
-    }else {
-        Player.speed = 10f;/*戻す*/
+
+    private void Start()
+    {
+        Player = GameObject.FindObjectOfType<Player>();
     }
 
-    if (pianochange_jumppower == 1) {
-        Player.jumppower = jumppower_p;
-    }else {
-        Player.jumppower = 250;/*戻す*/
+    private void Update()
+    {
+        if (pianochange_speed == 1)
+        {
+            Player.speed = speed_p;
+        }
+        else
+        {
+            Player.speed = 10f;/*戻す*/
+        }
+
+        if (pianochange_jumppower == 1)
+        {
+            Player.jumppower = jumppower_p;
+        }
+        else
+        {
+            Player.jumppower = 250;/*戻す*/
+        }
     }
+    
 
 }
 
