1a41cfcc674fab56651403df662b94725d53da6e
lumi <136560713+Lumi-snow@users.noreply.github.com>
2024-09-14 17:29:56 +0900

    動きの変更による画像の切り替え
    ジャンプアニメーションかなりゴリ押し。ジャンプ中の左右移動を直したい。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,19 @@ public class Player : MonoBehaviour
     public GameObject respornPoint;
     private Rigidbody rb;
 
+　　//動作によって表示するオブジェクトを切り替える。モーションの数だけオブジェクトを用意する。
+    public GameObject walk;
+    public GameObject standing;
+    public GameObject jump;
+
+    public bool isJumping = false;//ジャンプ中であるかどうか(isGroundとは違う扱い)
+
     void Start()
     {
         respornPoint = GameObject.Find("respornPoint");
         rb = GetComponent<Rigidbody>();
         isGround = false;
+        isJumping = false;
     }
 
     void Update()
@@ -26,17 +34,43 @@ public class Player : MonoBehaviour
         {
             movement += Vector3.left;
             //movement += new Vector3(-1, 0, 0); 左 (X軸方向)
+
+            //左向きに歩くアニメーション
+            standingState(false);
+            walkState(true);
+            transform.localScale = new Vector3(-1, 1, 1);
         }
-        if (Input.GetKey(KeyCode.D))
+
+        else if (Input.GetKey(KeyCode.D))
         {
             movement += Vector3.right;
             //movement += new Vector3(1, 0, 0);  右 (X軸方向)
             //movement += new Vector3(1, 0, 0);  右 (X軸方向)
+
+            //右向きに歩くアニメ―ション
+            standingState(false);
+            walkState(true);
+            transform.localScale = new Vector3(1, 1, 1);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        else if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            Invoke("Jump", 0.6f);//ジャンプの溜めの時間分呼び出しを遅らせる
+            isJumping = true;
+            Invoke("isJump", 1.9f);
+        }
+
+        else if (isJumping)//ジャンプのアニメ―ション
+        {
+            standingState(false);
+            jumpState(true);
+        }
+
+        else//何もせず地上にいる状態はstandingアニメーションを動かす。
+        {
+            standingState(true);
+            walkState(false);
+            jumpState(false);
         }
 
         rb.AddForce(movement * speed);
@@ -68,6 +102,24 @@ public class Player : MonoBehaviour
     public void playerResporn() {
         rb.velocity = new Vector3(0, 0, 0);
         this.transform.position = respornPoint.transform.position;
+    }
+
+    void standingState(bool nowState)//立ち状態
+    {
+        standing.SetActive(nowState);
+    }
+    void walkState(bool nowState)//歩き状態
+    {
+        walk.SetActive(nowState);
+    }
+    void jumpState(bool nowState)//ジャンプ状態
+    {
+        jump.SetActive(nowState);
+    }
+
+    void isJump()
+    {
+        isJumping = false;
     }
 }
 
