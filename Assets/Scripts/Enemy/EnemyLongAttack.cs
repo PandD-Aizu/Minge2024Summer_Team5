3eb886c6a6dc9d84d4b601efb48989a81ffc1a97
komamomiji <yokohirahazuki0607@gmail.com>
2024-09-19 17:11:14 +0900

    遠距離攻撃の弾を作成しました。

--- Assets/Scripts/Enemy/EnemyLongAttack.cs
+++ Assets/Scripts/Enemy/EnemyLongAttack.cs
@@ -9,7 +9,7 @@ public class EnemyLongAttack : MonoBehaviour
     private Vector3 rightshoot;
     private Vector3 leftshoot;
     private Quaternion rot;
-    private float power = 10;
+    [SerializeField] private float power = 500;
     private float shoottime = 1.0f;
     private float returntime = 3.0f;
     private float timelapse;
@@ -20,7 +20,7 @@ public class EnemyLongAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("start");
+
     }
  
     // Update is called once per frame
@@ -48,7 +48,7 @@ public class EnemyLongAttack : MonoBehaviour
                 {
                     rot = Quaternion.Euler(0f, 0f, -90f);
                     bullet = Instantiate(bulletPrefab, this.gameObject.transform.position + rightshoot, rot);
-                    bullet.GetComponent<Rigidbody>().AddForce(Vector3.up * power);
+                    bullet.GetComponent<Rigidbody>().AddForce(Vector3.right * power);
                 }
 
                 //左に生成して打ち出す
@@ -56,7 +56,7 @@ public class EnemyLongAttack : MonoBehaviour
                 {
                     rot = Quaternion.Euler(0f, 0f, 90f);
                     bullet = Instantiate(bulletPrefab, this.gameObject.transform.position + leftshoot, rot);
-                    bullet.GetComponent<Rigidbody>().AddForce(transform.up * power);
+                    bullet.GetComponent<Rigidbody>().AddForce(Vector3.left* power);
                 }
             }
         }
