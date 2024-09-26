a77238fb88260abef6c6ca90a37cfc363c70a5f9
carelessmth <shiganobita211293@gmail.com>
2024-09-26 14:52:46 +0900

    AntiGravityZone

---
+++ Assets/Scripts/AntiGravityZone.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+public class gravityController : MonoBehaviour
+{
+    public GameObject Player;
+    public Player aiueo;/*いい名前が思いつかない*/
+    public Vector3 Anti_Gravity;
+    public Vector3 Normal_Gravity;
+    public int InZoneChecker = 0;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+        aiueo = Player.GetComponent<Player>();
+        rb = Player.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        setLocalGravity();
+    }
+
+    void OnTriggerStay(Collider other)/*オブジェクトが重なっている間*/
+    {
+        Debug.Log("Trigger Stay: " + other.gameObject.name);
+        InZoneChecker = 1;
+    }
+    void OnTriggerExit(Collider other)/*オブジェクトが離れた時*/
+    {
+        Debug.Log("Trigger Exit: " + other.gameObject.name);
+        InZoneChecker = 0;
+    }
+
+
+    void setLocalGravity()
+    {
+        if(InZoneChecker == 1)
+        {
+            rb.AddForce(Anti_Gravity, ForceMode.Acceleration);
+            Debug.Log("G:Anti");
+        }
+        else if(InZoneChecker == 0)
+        {
+            rb.AddForce(Normal_Gravity, ForceMode.Acceleration);
+            Debug.Log("G:Normal");
+        }
+        
+
+    }
+
+}
+
