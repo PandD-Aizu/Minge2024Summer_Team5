befab8011d83d1999714130edbc08f3d66b857eb
komamomiji <yokohirahazuki0607@gmail.com>
2024-10-08 06:09:52 +0900

    ステージ１−３、２−１の作成
    ２−１のギミックで使用する丸太（Log）のPrehubを作成。
    ドラムで丸太を破壊できるようにしました。
    丸太を一定間隔で生成するLogDropと、丸太を消すエリアLogDestroyを作成しました。

---
+++ Assets/Scripts/StageGimmick/Log.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "LogDestroy")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Dsphere"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
