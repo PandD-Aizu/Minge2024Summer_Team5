678d36a16deebb8552a9f424d5435465962f923e
wasabi <hayato-f@i.softbank.jp>
2024-10-12 02:14:17 +0900

    TutorialStageのUpdate

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -33,6 +33,7 @@ public class Bullet : MonoBehaviour
         { 
             Destroy(this.gameObject);
         }
+
             
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -216,7 +216,10 @@ public class Player : MonoBehaviour
         }
         rb.velocity = new Vector3(0, 0, 0);
         currenthp = maxhp;
-        timer.remainingTime = timer.totalTime;
+        if (timer) {
+            timer.remainingTime = timer.totalTime;
+        }
+        
         this.transform.position = respornPoint.transform.position;
     }
 
