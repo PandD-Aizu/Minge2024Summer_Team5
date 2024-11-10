77e54105e675525d7a9b35ed94bf6d8cf6609261
komamomiji <yokohirahazuki0607@gmail.com>
2024-11-10 16:11:06 +0900

    ピアノのチュートリアルステージを作成しました。

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -162,8 +162,11 @@ public class Player : MonoBehaviour
         {
             instrument.isPianoAvailable = true;
             instrument.InstrumentInit();
-            instruments = timer.Instrument;
-            Destroy(instruments);
+            if (timer != null)
+            {
+                instruments = timer.Instrument;
+            }
+            Destroy(collision.gameObject);
         }
     }
 
